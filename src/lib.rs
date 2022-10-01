a94226066541ea7845b28ddb0f9830c82dccbaa2
carlos-rian <crian.rian@gmail.com>
2022-10-01 15:19:23 -0300

    include class error

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod conn;
 pub mod record;
 pub mod test_conn;
 pub mod value;
+use base::error::PysqlxDBError;
 use conn::PyConnection;
 use conn::_connect;
 use pyo3::prelude::*;
@@ -24,5 +25,6 @@ pub fn connect<'a>(py: Python<'a>, uri: String) -> Result<&'a PyAny, pyo3::PyErr
 #[pymodule]
 fn pysqlx_core(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(connect, m)?)?;
+    m.add_class::<PysqlxDBError>()?;
     Ok(())
 }
