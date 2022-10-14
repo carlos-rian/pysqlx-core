541feae30ddae6612647458e920bbfa3201511d9
carlos-rian <crian.rian@gmail.com>
2022-10-14 12:37:44 -0300

    test with serializer

--- test.py
+++ test.py
@@ -1,6 +1,38 @@
 import asyncio
-from time import time
+from time import time as now
 import pysqlx_core
+
+
+from datetime import date, datetime, time
+from decimal import Decimal
+from typing import Any, Dict, List
+from uuid import UUID
+
+from pydantic import Json, create_model, parse_obj_as
+
+
+TYPES = {
+    "int": int,
+    "bigint": int,
+    "float": float,
+    "double": float,
+    "string": str,
+    "bool": bool,
+    "char": str,
+    "decimal": Decimal,
+    "json": Json,
+    "uuid": UUID,
+    "datetime": datetime,
+    "date": date,
+    "time": time,
+    "array": list,
+    "xml": str,
+    # not implement
+    "bytes": Any,  # bytes
+    "enum": Any,  # Enum
+    "null": Any,
+}
+
 
 async def main():
     conn = await pysqlx_core.new(uri="postgresql://postgres:password@localhost:5432/fastapi_prisma?schema=public")
@@ -9,7 +41,12 @@ async def main():
     all = rows.get_all()
     first = rows.get_first()
     types = rows.get_types()
+    for key, value in types.copy().items():
+        types.update({key: (TYPES.get(value, Any), None)})
+    model = create_model("Model", **types)
+    new = parse_obj_as(List[model], all)
+    print(new)
 
-ini = time()
+ini = now()
 asyncio.run(main())
-print(time() - ini)
+print(now() - ini)
