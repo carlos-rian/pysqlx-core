35f87d575f8000f790146ca00f812505dace2e30
carlos-rian <crian.rian@gmail.com>
2022-10-19 22:56:32 -0300

    progress

--- types/src/py_types.rs
+++ types/src/py_types.rs
@@ -1,6 +1,6 @@
 use pyo3::types::PyString;
 use pyo3::{prelude::*, types::PyByteArray};
-
+use quaint::Value;
 type PyValueList = Vec<PyValue>;
 
 pub enum PyValue {
@@ -34,4 +34,34 @@ pub enum PyValue {
     Null,
 }
 
-impl PyValue {}
+impl From<Value> for PyValue {
+    fn from(value: Value) -> Self {
+        let py = Python::with_gil(|py| py);
+        match value {
+            Value::Boolean(Some(b)) => PyValue::Boolean(b),
+            Value::Boolean(None) => PyValue::Null,
+            Value::String(s) => PyValue::String(s),
+            Value::Enum(s) => PyValue::Enum(s),
+            Value::Enum(s) => s
+                .map(|s| PyValue::Enum(s.into_owned()))
+                .unwrap_or(PyValue::Null),
+            Value::Int(i) => PyValue::Int(i),
+            Value::List(l) => {
+                let mut list = Vec::new();
+                for item in l {
+                    list.push(PyValue::from(item));
+                }
+                PyValue::List(list)
+            }
+            Value::Json(s) => PyValue::Json(PyString::new(py, s)),
+            Value::Xml(s) => PyValue::Xml(PyString::new(py, s)),
+            Value::Uuid(s) => PyValue::Uuid(PyString::new(py, s)),
+            Value::Time(s) => PyValue::Time(PyString::new(py, s)),
+            Value::Date(s) => PyValue::Date(PyString::new(py, s)),
+            Value::DateTime(s) => PyValue::DateTime(PyString::new(py, s)),
+            Value::Float(s) => PyValue::Float(PyString::new(py, s)),
+            Value::Bytes(b) => PyValue::Bytes(PyByteArray::new(py, b)),
+            Value::Null => PyValue::Null,
+        }
+    }
+}
