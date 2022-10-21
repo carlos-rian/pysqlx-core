f75f9b8c39a1a4c3f2d8cd6172ca88c8453714c4
carlos-rian <crian.rian@gmail.com>
2022-10-21 12:13:24 -0300

    update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ fn new<'a>(py: Python<'a>, uri: String) -> Result<&'a PyAny, pyo3::PyErr> {
     pyo3_asyncio::tokio::future_into_py(py, async move {
         match Connection::new(uri).await {
             Ok(r) => Ok(r),
-            Err(e) => return Err(e.to_pyerr()),
+            Err(e) => Err(e.to_pyerr()),
         }
     })
 }
