10d6b1a5e5d1db0685fe4e965289782c0ea52195
carlos-rian <crian.rian@gmail.com>
2023-01-16 11:13:24 -0300

    remove env_logger, unnecessary.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use database::Connection;
-use env_logger;
+//use env_logger;
 use py_types::{PySQLXError, PySQLXResult};
 use std::io::Write;
 
@@ -28,9 +28,9 @@ fn new(py: Python, uri: String) -> PyResult<&PyAny> {
 
 #[pymodule]
 fn pysqlx_core(_py: Python, m: &PyModule) -> PyResult<()> {
-    env_logger::builder()
-        .format(|buf, record| writeln!(buf, "{}: {}", record.level(), record.args()))
-        .init();
+    //env_logger::builder()
+    //    .format(|buf, record| writeln!(buf, "{}: {}", record.level(), record.args()))
+    //    .init();
 
     m.add("__version__", get_version())?;
     m.add_function(wrap_pyfunction!(new, m)?)?;
