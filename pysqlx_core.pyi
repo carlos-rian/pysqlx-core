280a24a8512d3f44d956983b947c6d93c0908508
carlos.rian <carlos.rian@quartile.com>
2024-07-25 19:23:56 -0300

    Refactor Connection methods to accept optional query parameters

--- pysqlx_core.pyi
+++ pysqlx_core.pyi
@@ -119,16 +119,16 @@ class Connection:
     ```
     """
 
-    async def query(self, sql: str, params: tuple) -> "PySQLxResult":
+    async def query(self, sql: str, params: Union[tuple, None]) -> "PySQLxResult":
         """Returns a `PySQLxResult` object representing the result of the query."""
         raise PySQLxError()
-    async def execute(self, sql: str, params: tuple) -> "int":
+    async def execute(self, sql: str, params: Union[tuple, None]) -> "int":
         """Executes a query and returns the number of rows affected."""
         raise PySQLxError()
-    async def query_as_list(self, sql: str, params: tuple) -> "List[Dict[str, Any]]":
+    async def query_as_list(self, sql: str, params: Union[tuple, None]) -> "List[Dict[str, Any]]":
         """Returns a list of dictionaries representing the rows of the query result."""
         raise PySQLxError()
-    async def query_first_as_dict(self, sql: str, params: tuple) -> "Dict[str, Any]":
+    async def query_first_as_dict(self, sql: str, params: Union[tuple, None]) -> "Dict[str, Any]":
         """Returns the first row of the query result as a dictionary."""
         raise PySQLxError()
     async def raw_cmd(self, sql: str) -> "None":
