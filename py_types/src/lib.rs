940be63950d0b8491f223b57795d968b009b3774
carlos.rian <carlos.rian@quartile.com>
2024-07-30 21:02:28 -0300

    Refactor PySQLxParamKind enum to include 'dict' and 'list' as JSON types

--- py_types/src/lib.rs
+++ py_types/src/lib.rs
@@ -1,10 +1,8 @@
 mod errors;
 mod rows;
-mod sql;
 mod types;
 
 // re-export
 pub use errors::{py_error, DBError, PySQLxError};
 pub use rows::{PySQLxColumnTypes, PySQLxResult, PySQLxRow, PySQLxRows};
-pub use sql::prepare_sql_typed;
-pub use types::{convert_to_pysqlx_value, convert_to_quaint_values, PySQLxParamKind, PySQLxValue};
+pub use types::{PySQLxParamKind, PySQLxStatement, PySQLxValue};
