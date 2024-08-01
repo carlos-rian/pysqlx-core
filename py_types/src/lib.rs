146e68c8cbd58c39ea347932bc5d2721e3d2ff13
carlos.rian <carlos.rian@quartile.com>
2024-08-01 14:41:43 -0300

    Refactor PySQLxRows import to use PySQLxResponse instead of PySQLxResult

--- py_types/src/lib.rs
+++ py_types/src/lib.rs
@@ -1,8 +1,9 @@
 mod errors;
 mod rows;
 mod types;
+mod param;
 
 // re-export
 pub use errors::{py_error, DBError, PySQLxError, PySQLxInvalidParamError};
-pub use rows::{PySQLxColumnTypes, PySQLxResult, PySQLxRow, PySQLxRows};
+pub use rows::{PySQLxColumnTypes, PySQLxResponse, PySQLxRow, PySQLxRows};
 pub use types::{PySQLxParamKind, PySQLxStatement, PySQLxValue};
